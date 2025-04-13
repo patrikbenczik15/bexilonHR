7d570c815de89c0fc8d4592beec8a3a12808f44e
Patrik Benczik <benczikp.roberto@gmail.com>
2025-04-13 20:38:05 +0300

    nuked codebase

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
-import theme from './theme';
 import SignIn from './components/SignInSide';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -10,5 +9,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <CssBaseline />
       <SignIn />
     </ThemeProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
