9199cee6b4e3a47cf27f4d2980bc3b9046c1c8e8
Patrik Benczik <benczikp.roberto@gmail.com>
2025-04-21 03:26:08 +0300

    changed entry point name for no conflicts

--- backend/src/routes/UserRoute.ts
+++ backend/src/routes/UserRoute.ts
@@ -19,4 +19,10 @@ router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
 router.get("/:id/documents", getUserDocuments);
 router.get("/:id/documentRequests", getUserDocumentRequests);
+// !!! TODO route for assignedDocuments
+
+// TODO ? (maybe) router.get("/search", searchUsers);
+// * ^^^    ?role=HR&name=sabrina -> exemplu
+// TODO router.get("/me", getCurrentUser); cu JWT pt dashboard
+
 export default router;
